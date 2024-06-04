bd6e07da5e0cf1e3dce010060355556f50403127
AchingRCP <joseramoncaravaca@gmail.com>
2024-06-04 20:50:41 +0200

    creo funciones de operaciones y variables para numeros y operadores

---
+++ main.js
@@ -0,0 +1,22 @@
+function sum(a,b){
+    return a + b
+};
+
+function rest(a,b){
+    return a - b
+};
+
+function power(a,b){
+    return a * b
+};
+
+function divide(a,b){
+    return a/b
+};
+
+let firstNumber = ""
+let secondNumber = ""
+let operator =""
+
+
+
